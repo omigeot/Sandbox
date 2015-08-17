73aa6a0aa03021ecc43c0a79ccd2d8a41c151aef
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2015-08-17 16:44:34 -0400

    Added per-request editorview sass compilation

--- support/server/sandboxAPI.js
+++ support/server/sandboxAPI.js
@@ -4,7 +4,9 @@ var libpath = require('path'),
 	url = require("url"),
 	mime = require('mime'),
 	sio = require('socket.io'),
-	YAML = require('js-yaml');
+	YAML = require('js-yaml'),
+	sass = require('node-sass');
+
 require('./hash.js');
 var _3DR_proxy = require('./3dr_proxy.js');
 var safePathRE = RegExp('/\//' + (libpath.sep == '/' ? '\/' : '\\') + '/g');
@@ -1781,6 +1783,24 @@ function serve(request, response)
 						else {
 							response.send(global.configuration.remoteAssetServerURL);
 						}
+					}
+					break;
+				case "geteditorcss":
+					{
+						sass.render({
+							file: libpath.join(__dirname, '../client/lib/vwf/view/editorview/css/Editorview.scss'),
+							includePaths: [libpath.join(__dirname, '../client/lib/vwf/view/editorview/css/')],
+							sourceComments: true
+						}, function(err,result){
+							if(err){
+								console.error('Error compiling sass:', err);
+								response.sendStatus(500);
+							}
+							else {
+								response.set('Content-Type', 'text/css');
+								response.send(result.css);
+							}
+						});
 					}
 					break;
 				default:
