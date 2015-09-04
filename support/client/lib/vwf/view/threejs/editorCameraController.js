1618d4d8af98be44ef7309fb9663bbc7687efa2b
rchadwic <rchadwic@gmail.com>
2015-09-04 16:02:33 -0400

    compute near and far plane better

--- support/client/lib/vwf/view/threejs/editorCameraController.js
+++ support/client/lib/vwf/view/threejs/editorCameraController.js
@@ -58,10 +58,8 @@ define(["vwf/view/threejs/oldCameraController", "vwf/view/threejs/AvatarCameraCo
         $('#index-vwf')[0].addEventListener("touchmove", this.localTouchMove.bind(this), true);
         this.prerendercallback = this.prerender.bind(this);
         _dView.bind('prerender', this.prerendercallback);
-
         this.postrendercallback = this.postrender.bind(this);
         _dView.bind('postrender', this.postrendercallback);
-
         this.updateCamera();
     }
     editorCameraController.prototype.addController = function(name, controller)
@@ -72,22 +70,128 @@ define(["vwf/view/threejs/oldCameraController", "vwf/view/threejs/AvatarCameraCo
     {
         return this.cameraControllers[name];
     }
-     editorCameraController.prototype.prerender = function(e)
+    editorCameraController.prototype.prerender = function(e, vp, h, w)
     {
         if (!this.cameraControllers[this.cameramode]) return;
         this.cameraControllers[this.cameramode].prerender(e)
-        var hit = _SceneManager.CPUPick(_Editor.getCameraPosition(), _Editor.GetCameraCenterRay());
-        if (hit)
+            //lets construct a screenspace rect of the selection region
+        var top = 0;
+        var left = 0
+        var bottom = h;
+        var right = w;
+       
+        var TopLeftRay = _Editor.GetWorldPickRay(
         {
-            this.camera.near = hit.distance / 5;
-            this.camera.far = Math.max(5000, hit.distance * 3);
-            this.camera.updateProjectionMatrix();
-        }
-        else
+            clientX: left,
+            clientY: top
+        });
+        var TopRightRay = _Editor.GetWorldPickRay(
         {
-            this.camera.near = .01;;
-            this.camera.far = 5000;
-            this.camera.updateProjectionMatrix();
+            clientX: right,
+            clientY: top
+        });
+        var BottomLeftRay = _Editor.GetWorldPickRay(
+        {
+            clientX: left,
+            clientY: bottom
+        });
+        var BottomRighttRay = _Editor.GetWorldPickRay(
+        {
+            clientX: right,
+            clientY: bottom
+        });
+        //now we build a frustum from the screenspace rect and the camera
+        var campos = _Editor.getCameraPosition();
+        var ntl = MATH.addVec3(campos, TopLeftRay);
+        var ntr = MATH.addVec3(campos, TopRightRay);
+        var nbl = MATH.addVec3(campos, BottomLeftRay);
+        var nbr = MATH.addVec3(campos, BottomRighttRay);
+        var ftl = MATH.addVec3(campos, MATH.scaleVec3(TopLeftRay, 10000));
+        var ftr = MATH.addVec3(campos, MATH.scaleVec3(TopRightRay, 10000));
+        var fbl = MATH.addVec3(campos, MATH.scaleVec3(BottomLeftRay, 10000));
+        var fbr = MATH.addVec3(campos, MATH.scaleVec3(BottomRighttRay, 10000));
+        var frustrum = new Frustrum(ntl, ntr, nbl, nbr, ftl, ftr, fbl, fbr);
+        //get all the objects intersected
+        var hits = _SceneManager.FrustrumCast(frustrum,
+        {
+            OneHitPerMesh: true,
+            cullQuery: true
+        });
+        var htl = _SceneManager.CPUPick(campos, TopLeftRay,
+        {
+            OneHitPerMesh: true,
+            cullQuery: true
+        });
+        var htr = _SceneManager.CPUPick(campos, TopRightRay,
+        {
+            OneHitPerMesh: true,
+            cullQuery: true
+        });
+        var hbl = _SceneManager.CPUPick(campos, BottomLeftRay,
+        {
+            OneHitPerMesh: true,
+            cullQuery: true
+        });
+        var hbr = _SceneManager.CPUPick(campos, BottomRighttRay,
+        {
+            OneHitPerMesh: true,
+            cullQuery: true
+        });
+        if (hbr) hits.push(hbr);
+        if (hbl) hits.push(hbl);
+        if (htr) hits.push(htr);
+        if (htl) hits.push(htl);
+        if (hits.length)
+        {
+            var objects = {};
+            for (var i in hits)
+            {
+                if (!objects[hits[i].object.uuid])
+                    objects[hits[i].object.uuid] = {
+                        object: hits[i].object,
+                        hits: []
+                    }
+                objects[hits[i].object.uuid].hits.push(hits[i])
+            }
+            var intersections = []
+            for (var i in objects)
+            {
+                var oneInFront = false;
+                var ssPoints = [];
+                for (var j in objects[i].hits)
+                {
+                    var screenspacehit = MATH.mulMat4Vec3(vp, objects[i].hits[j].point);
+                    if (isFinite(screenspacehit[2]) || !isNaN(screenspacehit[2]))
+                    {
+                        if (screenspacehit[2] > 0)
+                        {
+                            ssPoints.push(screenspacehit)
+                            oneInFront = true;
+                        }
+                    }
+                }
+                if (oneInFront)
+                    intersections = intersections.concat(ssPoints)
+            }
+            var near = Infinity;
+            var far = -Infinity;
+            for (var i in intersections)
+            {
+                if (!isFinite(intersections[i][2]) || isNaN(intersections[i][2])) continue;
+                if (intersections[i][2] < near)
+                    near = intersections[i][2];
+                if (intersections[i][2] > far)
+                    far = intersections[i][2];
+            }
+            near = Math.max(.001, near);
+            console.log(near, far);
+            //far = Math.min(.001,near);
+            if (isFinite(near) && isFinite(far))
+            {
+                this.camera.near = near/2;
+                this.camera.far = far * 2;
+                this.camera.updateProjectionMatrix();
+            }
         }
     }
     editorCameraController.prototype.postrender = function(e)
