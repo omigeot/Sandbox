cf8ec7efc9b87ea4a01a0cb404e6a678322d08ac
rchadwic <rchadwic@gmail.com>
2015-10-04 14:09:50 -0400

    gerstner waves with sum-of-sines normals

--- public/adl/sandbox/ocean/ocean.vert.glsl
+++ public/adl/sandbox/ocean/ocean.vert.glsl
@@ -4,6 +4,8 @@ vec3 sundir = vec3(.5, .5, 1);
 uniform float t;
 #define numWaves 9
 #define PI 3.1415926535897932384626433832795
+
+float gA = .5;
 float L[numWaves];
 float A[numWaves];
 float S[numWaves];
@@ -47,25 +49,44 @@ void main() {
 
       float dxH = 0.0;
       float dyH = 0.0;
+
+      float x1 = x;
+      float y1 = y;
+
       for (int i = 0; i < numWaves; i++)
       {
             float w =  2.0 * PI / L[i];
-            A[i] = 0.5/(w * 2.718281828459045);
-            S[i] = PI/(w * 2.718281828459045);
+            A[i] = 0.5/(w * 2.718281828459045); //for ocean on Earth, A is ususally related to L
+            S[i] = PI/(w * 2.718281828459045);  //for ocean on Earth, S is ususally related to L
             float q = S[i] * w;
            
             vec2 xy = vec2(x, y);
-            float hi = A[i] * sin( dot(D[i], xy) * w + t * q);
-            h += hi;
+
+            // simple sum-of-sines
+            //float hi = A[i] * sin( dot(D[i], xy) * w + t * q);
+            //h += hi * gA;
            
             float dxHi = w * D[i].x * A[i] * cos(     dot(D[i],xy)*w + t * q );
             float dyHi = w * D[i].y * A[i] * cos(     dot(D[i],xy)*w + t * q );
 
-            dxH += dxHi;
-            dyH += dyHi;
+            dxH += dxHi * gA;
+            dyH += dyHi * gA;
+            
+            //Gerstner
+
+            float Q = 2.0;
+            float Qi = Q/(w*A[i]*float(numWaves)); // *numWaves?
+            float xi = Qi * A[i] * D[i].x * cos( dot(w*D[i],xy) + q*t);
+            float yi = Qi * A[i] * D[i].y * cos( dot(w*D[i],xy) + q*t);
+            float hi = A[i] * sin( dot(w*D[i],xy) + q * t );
+
+            x1 += xi;
+            y1 += yi;
+            h += hi;
+
 
       }
-      vec3 tPos = vec3(position.x, position.y, h);
+      vec3 tPos = vec3(x1, y1, h);
       vec3 tNormal = normalize(vec3(-dxH, -dyH, 1));
       vNormal = normalMatrix * normalize(tNormal);
       vSundir = normalMatrix * normalize(sundir);
