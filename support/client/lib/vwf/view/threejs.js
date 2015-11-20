a24c77280b7274e1c80a64650ba6fa8aeae900cf
rchadwic <rchadwic@gmail.com>
2015-11-20 17:29:41 -0500

    don't fail if publishsettings is null (though it never should be)

--- support/client/lib/vwf/view/threejs.js
+++ support/client/lib/vwf/view/threejs.js
@@ -710,7 +710,7 @@ define(["module", "vwf/view", "vwf/model/threejs/OculusRiftEffect", "vwf/model/t
 
                 var instanceData = _DataManager.getInstanceData();
                 var    publishSettings = instanceData.publishSettings;
-
+                if(publishSettings)
                 this.cameraID = publishSettings.camera;
                 this.setCamera(this.cameraID);
             }
@@ -820,6 +820,7 @@ define(["module", "vwf/view", "vwf/model/threejs/OculusRiftEffect", "vwf/model/t
             this.cameraID = camID  || defaultCameraID;
 
             //allow the default 
+            if(publishSettings)
             if(!camID && publishSettings.allowTools)
                 this.cameraID = null;
 
