d854e0980e6d0d5fb35041db4beaa14b49367467
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2016-01-26 16:52:18 -0500

    Started on folder view

--- support/client/lib/vwf/view/editorview/mapbrowser.js
+++ support/client/lib/vwf/view/editorview/mapbrowser.js
@@ -2,9 +2,44 @@
 
 define(['./angular-app', './manageAssets'], function(app)
 {
+	app.service('TextureDataManager', ['$http', function($http)
+	{
+		var data = [];
+
+		function isObject(x){
+			return x.name && x.contents;
+		}
+
+		function azFolderFile(a, b)
+		{
+			if( isObject(a) && isObject(b) ){
+				return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
+			}
+			else if( isObject(a) && !isObject(b) ){
+				return -1;
+			}
+			else if( !isObject(a) && isObject(b) ){
+				return 1;
+			}
+			else {
+				return a.toLowerCase() < b.toLowerCase() ? -1 : 1;
+			}
+		}
+
+		$http.get('./vwfdatamanager.svc/textures', {}).then(
+			function success(response){
+				data.push.apply(data, response.data);
+				data.sort(azFolderFile);
+			},
+			function error(response){}
+		);
+
+		return data;
+	}]);
+
 	var textureCallback = null;
 
-	app.controller('MapBrowserController', ['$scope','AssetDataManager', function($scope, UserAssets)
+	app.controller('MapBrowserController', ['$scope','AssetDataManager','TextureDataManager', function($scope, UserAssets, TexList)
 	{
 		$('#MapBrowser').dialog({
 			title: 'Map Browser',
@@ -23,9 +58,38 @@ define(['./angular-app', './manageAssets'], function(app)
 			minWidth: 283
 		});
 
+		$scope.home = [
+			{name: 'My Assets', contents: []},
+			{name: 'Scene', contents: []},
+			{name: 'Server', contents: TexList}
+		];
 
-		$scope.breadcrumbs = ['jungle_1','jungle','asdf','jklk','Dust In The Wind'];
+		//$scope.serverTex = TexList;
+		//$scope.assets = UserAssets;
+
+		$scope.breadcrumbs = ['Server'];
 		$scope.view = 'thumbs'; // or 'list'
+
+		$scope.followPath = function(crumbs, folder)
+		{
+			var base = folder || $scope.home;
+			if(crumbs.length === 0){
+				return base;
+			}
+			else
+			{
+				for(var i=0; i<base.length; i++){
+					if(base[i].name === crumbs[0])
+						return $scope.followPath(crumbs.slice(1), base[i].contents);
+				}
+				return [];
+			}
+		};
+
+		$scope.getUID = function(item, crumbs)
+		{
+			return crumbs.slice(1).join('/') + '/' + item;
+		}
 	}]);
 
 
