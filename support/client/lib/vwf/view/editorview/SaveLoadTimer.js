51c3810d5340522e105b8d6fd3b9944e1e51b0a5
Chadwick <Chadwick, Robert C>
2016-06-01 08:38:17 -0700

    fix prompt to auto save on leave

--- support/client/lib/vwf/view/editorview/SaveLoadTimer.js
+++ support/client/lib/vwf/view/editorview/SaveLoadTimer.js
@@ -3,21 +3,23 @@ define({
 	{
 		
 		
-		
+	
 		//don't even start the timer for published worlds
 		if(_DataManager.getInstanceData().publishSettings.persistence)
 			window.setTimeout(function(){_DataManager.saveTimer();},60000);	
 
 		if(_DataManager.getInstanceData().publishSettings.persistence)
 		{
-		 window.onbeforeunload += function(){
-			//user must exist
-			if(_UserManager.GetCurrentUserName() && _DataManager.getInstanceData().publishSettings.persistence)
-			{
-				_DataManager.saveToServer(true);
-				return "Are you sure you want to leave this Sandbox world?";
-			}		
-		};
+			window.onbeforeunload = function(e){
+				//user must exist
+				if(_UserManager.GetCurrentUserName() && _DataManager.getInstanceData().publishSettings.persistence)
+				{
+					_DataManager.saveToServer(true);
+					var returnStr = "Are you sure you want to leave this Sandbox world?";
+					return returnStr;
+					e.returnValue = returnStr;
+				}		
+			};
 		}
 		$(window).unload(function ()
 		{
