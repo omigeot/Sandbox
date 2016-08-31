f31cb1f9ea0313c01d8caeb209c526fba63b02db
rchadwic <rchadwic@gmail.com>
2016-08-31 11:50:09 -0400

    fix minor bug in transform tools that makes the gizmo disappear

--- support/client/lib/vwf/view/editorview/transformTool.js
+++ support/client/lib/vwf/view/editorview/transformTool.js
@@ -376,6 +376,10 @@ var transformTool = function()
     }
     this.updateSize = function()
     {
+        if(isNaN(this.getGizmoBody().matrix.elements[0]))
+        {
+            this.getGizmoBody().matrix.copy(new THREE.Matrix4());
+        }
         var tgizpos = [0, 0, 0];
         var tgizpos2 = [0, 0, 0];
         var transposeTemp = [];
@@ -386,6 +390,7 @@ var transformTool = function()
         tgizpos[2] = this.getGizmoHead().matrixWorld.elements[14];
         var campos = _Editor.getCameraPosition();
         var dist = MATH.lengthVec3(Vec3.subtract(tgizpos, campos, tempvec1));
+        dist = Math.max(dist,.0001); //prevent 0 dist thus 0 scale thus NANs in matrix
         var cam = _Editor.findcamera();
         cam.updateMatrixWorld(true);
         var fovadj = cam.fov / 75;
