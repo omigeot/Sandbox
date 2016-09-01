a9b4549ca088f6f45566544c9b246f88cdd0c3cf
Chadwick <chadwickr@vacolab.adlnet.org>
2016-09-01 15:53:27 -0400

    fix sliders

--- support/client/lib/vwf/view/editorview/MaterialEditor.js
+++ support/client/lib/vwf/view/editorview/MaterialEditor.js
@@ -313,7 +313,7 @@ define(['./angular-app', './mapbrowser', './colorpicker', './EntityLibrary'], fu
 
 				// update the value when sliding
 				slider.on('slide', function(evt, ui){
-				//	var fn = $scope.change();
+					var fn = $scope.change && $scope.change(); //in somecases, there is not this func
 					var changedIndex = undefined;
 
 					if(rangeMode){
@@ -324,7 +324,7 @@ define(['./angular-app', './mapbrowser', './colorpicker', './EntityLibrary'], fu
 					else $scope.mantissa = ui.value;
 					$scope.$apply();
 
-					//if(fn) fn(changedIndex);
+					if(fn) fn(changedIndex);
 				});
 
 				// update sliding status
