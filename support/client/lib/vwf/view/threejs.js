d8a46758f98c9e37ccff8b0b244b03ba660bc446
rchadwic <rchadwic@gmail.com>
2016-12-14 11:11:56 -0500

    make the default ID for event generation use the ID of the active camera , if there is no object under the mouse.

--- support/client/lib/vwf/view/threejs.js
+++ support/client/lib/vwf/view/threejs.js
@@ -1076,7 +1076,12 @@ define(["module", "vwf/view", 'vwf/utility/eventSource', "vwf/view/threejs/viewN
                 self.trigger('pickStart');
                 var newPick = ThreeJSPick.call(self, sceneNode, cam, ww, wh);
 
-                var newPickId = newPick ? getPickObjectID.call(view, newPick.object) : view.state.sceneRootID;
+
+                var newPickId = null; 
+                if(newPick)
+                    newPickId = getPickObjectID.call(view, newPick.object);
+                if(!newPickId) 
+                    newPickId = _dView.cameraID ||  view.state.sceneRootID;
                 self.trigger('pickEnd');
 
                 if (self.lastPickId != newPickId && self.lastEventData) {
@@ -1106,8 +1111,9 @@ define(["module", "vwf/view", 'vwf/utility/eventSource', "vwf/view/threejs/viewN
                 } else if (self.lastEventData && self.mouseOverCanvas && !hovering && self.lastPick) {
                     var pickId = getPickObjectID.call(view, self.lastPick.object, false);
                     if (!pickId) {
-                        pickId = view.state.sceneRootID;
+                        pickId =  _dView.cameraID || view.state.sceneRootID;
                     }
+
                     view.kernel.dispatchEvent(pickId, "pointerHover", self.lastEventData.eventData, self.lastEventData.eventNodeData);
                     hovering = true;
                 }
@@ -1611,6 +1617,10 @@ define(["module", "vwf/view", 'vwf/utility/eventSource', "vwf/view/threejs/viewN
 
             threeActualObj = pickInfo ? pickInfo.object : undefined;
             pointerPickID = pickInfo ? getPickObjectID.call(sceneView, pickInfo.object, debug) : undefined;
+            if(!pointerPickID)
+            {
+                pointerPickID = _dView.cameraID || view.state.sceneRootID;
+            }
             var mouseButton = "left";
             switch (e.button) {
                 case 2:
